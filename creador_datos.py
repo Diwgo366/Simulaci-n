73169629e41fd89ec0c94fea8fb04702a3e3ab1a
Diego Chancef <diegoamirch@gmail.com>
2023-11-07 23:52:17 -0500

    Reorganizacion

--- creador_datos.py
+++ creador_datos.py
@@ -1,21 +1,61 @@
 import random
+import math
 import numpy as np
+import matplotlib.pyplot as plt
+import os
 
-NPAR = 60
+SISTEMA = 3
+NPAR = 50
 LARGO = 10
-VEL = 5
+VELMIN = -20
+VELMAX = 20
 
 coordenadas_unicas = set()
 while len(coordenadas_unicas) < NPAR:
     fila = (random.randint(0, LARGO), random.randint(0, LARGO))
-    coordenadas_unicas.add(fila)
+    if fila not in coordenadas_unicas:
+        coordenadas_unicas.add(fila)
 
 matriz1 = list(coordenadas_unicas)
 random.shuffle(matriz1)
+matriz2 = np.random.randint(0, LARGO, size=(NPAR, 1))
+matriz3 = np.random.randint(VELMIN, VELMAX, size=(NPAR, 3))
+matriz_final = np.concatenate((matriz1, matriz2, matriz3), axis=1)
+np.savetxt(f"Datos/Datos_{SISTEMA}.dat", matriz_final, fmt="%d", delimiter="\t")
 
-matriz2 = np.random.randint(-VEL, VEL+1, size=(NPAR, 2))
+print("Matriz guardada")
 
-matriz_final = np.concatenate((matriz1, matriz2), axis=1)
+velocidades_cuadradas = []
+for i in range(NPAR):
+    velocidad_cuadrada =  matriz_final[i,0]**2 + matriz_final[i,1]**2
+    velocidades_cuadradas.append(velocidad_cuadrada)
 
-np.savetxt("Datos.dat", matriz_final, fmt="%d", delimiter="\t")
-print("Matriz guardada en Datos.dat")
+maximo = max(velocidades_cuadradas)
+minimo = min(velocidades_cuadradas)
+rango = maximo - minimo
+Ninter = int(1 + 3.3*math.log(len(velocidades_cuadradas)))
+amplitud = rango/Ninter
+intervalos = [0] * Ninter
+frecuencia = [0] * Ninter
+media = sum(velocidades_cuadradas)/len(velocidades_cuadradas)
+for i in range(Ninter):
+    limite = minimo + i*amplitud
+    intervalos[i] = limite
+    cantidad = 0
+    for j in range(NPAR):
+        if velocidades_cuadradas[j] != "Na":
+            if velocidades_cuadradas[j] < limite:
+                cantidad = cantidad + 1
+                velocidades_cuadradas[j] = "Na"
+    frecuencia[i] = cantidad
+plt.bar(intervalos, frecuencia, width=amplitud*0.9, align='edge')
+plt.title('Grafica de dispersion de las velocidades')
+plt.xlabel('Velocidad al cuadrado')
+plt.ylabel('Frecuencia')
+plt.axvline(x=media, color='red', linestyle='--', linewidth=1)
+carpeta = f'Graficas/Sistema_{SISTEMA}'
+if not os.path.exists(carpeta):
+    os.makedirs(carpeta)
+ruta_imagen = os.path.join(carpeta, f"Dispersion_vel_ini_{SISTEMA}.png")
+plt.savefig(ruta_imagen)
+plt.show()
--- proyecto.py
+++ proyecto.py
@@ -3,97 +3,114 @@ import math
 import numpy as np
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
+import os
 
 # Declaración de parámetros
-TIEMPO = 1
+SISTEMA = 3
+TIEMPO = 10
 DT = 0.01
-IMAGENES = 3
+IMAGENES = 1
 LONGITUD = 10
 MASA = 1
 RADIO_CORTE = 4
 EPSILON = 1
 GAMA = 1
-ANIMAR = True
-RUTA_DATOS = "Datos.dat"
+ANIMAR = False
+RUTA_DATOS = f"Datos/Datos_{SISTEMA}.dat"
 
 # Obtención de condiciones iniciales
 datos = np.loadtxt(RUTA_DATOS)
 posicion_x = datos[:, 0]
 posicion_y = datos[:, 1]
-velocidad_x = datos[:, 2]
-velocidad_y = datos[:, 3]
+posicion_z = datos[:, 2]
+velocidad_x = datos[:, 3]
+velocidad_y = datos[:, 4]
+velocidad_z = datos[:, 5]
 distancia_media = []
+velocidad_cuadrada = []
 NPAR = len(posicion_x)
 PARES = NPAR*(NPAR-1)/2
 
-def graficar(vector, variable):
+def graficar(vector, variable, nombre):
     """Función para crear imagenes"""
     eje_x = list(range(len(vector)))
     eje_x = [i * DT for i in eje_x]
-    plt.scatter(eje_x, vector, marker='o', color='red', s=10)
+    carpeta = f'Graficas/Sistema_{SISTEMA}'
+    if not os.path.exists(carpeta):
+        os.makedirs(carpeta)
+    plt.scatter(eje_x, vector, marker='o', color='red', s=5)
     plt.xlabel('Tiempo')
     plt.ylabel(variable)
+    plt.title(f"Tiempo vs {variable}")
+    ruta_imagen = os.path.join(carpeta, f'{nombre}.png')
+    plt.savefig(ruta_imagen)
     plt.show()
 
-def actualizar_posiciones(act_pos_x, act_pos_y, act_vel_x, act_vel_y, part):
+def actualizar_posiciones(act_pos_x, act_pos_y, act_pos_z, act_vel_x, act_vel_y, act_vel_z, part):
     """Función para actualizar las posiciones"""
     fuerza_x = 0
     fuerza_y = 0
+    fuerza_z = 0
     distancia_media_parcial = 0
     for i in range(NPAR):
         distancia_x = posicion_x[part] - posicion_x[i]
         distancia_y = posicion_y[part] - posicion_y[i]
+        distancia_z = posicion_z[part] - posicion_z[i]
         distancia = math.sqrt(distancia_x**2 + distancia_y**2)
         if i > part:
             distancia_media_parcial = distancia_media_parcial + distancia
         if i != part and distancia <= RADIO_CORTE:
-            fuerza_x = fuerza_x + 24*EPSILON*GAMA**6 * \
-                (2*GAMA**6*distancia**(-14)-distancia**(-8))*distancia_x
-            fuerza_y = fuerza_y + 24*EPSILON*GAMA**6 * \
-                (2*GAMA**6*distancia**(-14)-distancia**(-8))*distancia_y
+            fuerza_x = fuerza_x + 24*EPSILON*GAMA**6 * (2*GAMA**6*distancia**(-14)-distancia**(-8)) * distancia_x
+            fuerza_y = fuerza_y + 24*EPSILON*GAMA**6 * (2*GAMA**6*distancia**(-14)-distancia**(-8)) * distancia_y
+            fuerza_z = fuerza_z + 24*EPSILON*GAMA**6 * (2*GAMA**6*distancia**(-14)-distancia**(-8)) * distancia_z
     acl_x = fuerza_x / MASA
     acl_y = fuerza_y / MASA
+    acl_z = fuerza_z / MASA
     act_vel_x = act_vel_x + acl_x * DT
     act_vel_y = act_vel_y + acl_y * DT
+    act_vel_z = act_vel_z + acl_z * DT
     act_pos_x = act_pos_x + act_vel_x * DT
     act_pos_y = act_pos_y + act_vel_y * DT
-    return act_pos_x, act_pos_y, distancia_media_parcial
+    act_pos_z = act_pos_z + act_vel_z * DT
+    return act_pos_x, act_pos_y, act_pos_z, distancia_media_parcial
 
 if ANIMAR:
     fig = plt.figure(figsize=(6, 6))
     grafico, = plt.plot([], [], "o", markersize=5)
     plt.xlim(0 - LONGITUD*(IMAGENES-1)/2, LONGITUD + LONGITUD*(IMAGENES-1)/2)
     plt.ylim(0 - LONGITUD*(IMAGENES-1)/2, LONGITUD + LONGITUD*(IMAGENES-1)/2)
-    for i in range(IMAGENES):
+    for i in range(IMAGENES-1):
         x = i * LONGITUD
         plt.axvline(x, color='red', linestyle='--', alpha=0.5)
-    for i in range(IMAGENES):
+    for i in range(IMAGENES-1):
         y = i * LONGITUD
         plt.axhline(y, color='red', linestyle='--', alpha=0.5)
     def animate(frame):
         """Funcion para animar"""
         distancia_media_total1 = 0
         for i in range(NPAR):
-            posicion_x[i], posicion_y[i], media_par1 = actualizar_posiciones(
-                posicion_x[i], posicion_y[i], velocidad_x[i], velocidad_y[i], i)
+            posicion_x[i], posicion_y[i], posicion_z[i], media_par1 = actualizar_posiciones(
+                posicion_x[i], posicion_y[i], posicion_z[i], velocidad_x[i], velocidad_y[i], velocidad_z[i], i)
             posicion_x[i] = posicion_x[i] % LONGITUD
             posicion_y[i] = posicion_y[i] % LONGITUD
+            posicion_z[i] = posicion_z[i] % LONGITUD
             distancia_media_total1 = distancia_media_total1 + media_par1
         distancia_media.append(distancia_media_total1/PARES)
         grafico.set_data(posicion_x, posicion_y)
         return grafico,
     ani = animation.FuncAnimation(
         fig, animate, frames=int(TIEMPO/DT), blit=True)
+    plt.title("Ejes x vs y")
     plt.show()
 else:
     for step in range(int(TIEMPO / DT)):
         distancia_media_total2 = 0
         for i in range(NPAR):
-            posicion_x[i], posicion_y[i], media_par2 = actualizar_posiciones(
-                posicion_x[i], posicion_y[i], velocidad_x[i], velocidad_y[i], i)
+            posicion_x[i], posicion_y[i], posicion_z[i], media_par2 = actualizar_posiciones(
+                posicion_x[i], posicion_y[i], posicion_z[i], velocidad_x[i], velocidad_y[i], velocidad_z[i], i)
             posicion_x[i] = posicion_x[i] % LONGITUD
             posicion_y[i] = posicion_y[i] % LONGITUD
+            posicion_z[i] = posicion_z[i] % LONGITUD
             distancia_media_total2 = distancia_media_total2 + media_par2
         distancia_media.append(distancia_media_total2/PARES)
-    graficar(distancia_media, "Distancia media")
-    
+    graficar(distancia_media, "Distancia media", f"Distancia_media_{SISTEMA}")
